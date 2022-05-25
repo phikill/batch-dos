03f77d0c8542e85f89117e8f3d0bd455eb17220e
Gustavo S Simões <72417648+phikill@users.noreply.github.com>
2022-05-24 21:28:52 -0300

    Create 1edilson.bat

---
+++ ATIVIDADES/1edilson.bat
@@ -0,0 +1,301 @@
+@echo off
+cls 
+
+color 1E
+Title Atividade Bonita
+echo  __________________________________
+echo / SCRIPT .BAT para realizar backup \
+echo !     apenas o basico do basico    !
+echo \__________________________________/
+echo            \
+echo             \  /_/
+echo               (oo)\_______
+echo               (__)\       )\/\
+echo                    !!---w !
+echo                    !!    !!
+pause
+cls
+
+echo  _____________________________________________  
+echo /Criando pasta /Edilson em Documentos publicos\
+echo -----------------------------------------------   
+
+cd \Users\Public\Documents
+mkdir Edilson
+cd \Users\Public\Documents\Edilson
+nul> EdilsonLog.txt
+
+
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+
+REM relaxa e apenas estetica
+
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 膊膊膊膊膊膊膊膊膊膊 1%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 鄄膊膊膊膊膊膊膊膊膊 2%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 鄄膊膊膊膊膊膊膊膊膊 3%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹膊膊膊膊膊膊膊膊膊 10%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹鄄膊膊膊膊膊膊膊膊 15%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹膊膊膊膊膊膊膊膊 20%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹鄄膊膊膊膊膊膊膊 25%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹膊膊膊膊膊膊膊 50%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹鄄膊膊膊膊膊膊 55%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹膊膊膊膊膊膊 40%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹鄄膊膊膊膊膊 45%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹膊膊膊膊膊 50%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹鄄膊膊膊膊 55%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹圹膊膊膊膊 60%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹圹鄄膊膊膊 65%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹圹圹膊膊膊 70%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹圹圹圹膊膊 80%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹圹圹圹鄄膊 85%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹圹圹圹圹膊 90%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹圹圹圹圹鄄 95%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo   ____________________________________________________
+echo  /Copiando Dados da Pasta /media para a pasta /Edilson\
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹圹圹圹圹圹 100%%
+echo ----------------------------------
+GOTO END
+:END
+cls
+echo   ____________________________________________________
+echo  /Progresso Concluido Pressione Alguma Tecla          \
+echo  ------------------------------------------------------
+echo.
+echo.
+echo Copiado!
+echo ----------------------------------
+echo Progress: 圹圹圹圹圹圹圹圹圹圹 100%%
+echo.
+echo.
+
+REM aqui termina
+
+cd \Windows\media
+copy tada.wav \Users\Public\Documents\Edilson
+pause
+
+echo  _____________________________
+echo /                             \
+echo !---Abrir o arquivo copiado---!
+echo \_____________________________/ 
+cd \Users\Public\Documents\Edilson
+start \Users\Public\Documents\Edilson/tada.wav
+start \Users\Public\Documents\Edilson
+pause
+
+echo salvando log do BAT...
+ (cd \Users\Public\Documents\Edilson whoami dir)>>EdilsonLog.txt
+
+
+REM for /l %%a in (1,1,50000) do (
+REM	cls
+REM	echo Esta tudo pronto: %%a...
+REM )
